0640c0ee5d231d9d6d8589770c5b8157861aff8b
ejkim1996 <ejkim1996@gmail.com>
2017-03-18 12:34:49 -0400

    Files for changing scenes

---
+++ MoveElevator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveElevator : MonoBehaviour {
+
+	public Transform groundFloor;
+	public Transform topFloor;
+
+	public GameObject player;
+
+	public float speed = 1.0F;
+	private float startTime;
+	private float totalDistance;
+
+	public bool isMoving = false;
+
+	void Start () {
+
+	}
+	
+	void Update () {
+
+		if (Input.GetKeyUp("space") && isMoving == false){
+			startTime = Time.time;
+			totalDistance = Vector3.Distance(groundFloor.position, topFloor.position);
+			isMoving = true;
+
+			player.transform.SetParent(transform);
+		}
+
+		float distCovered = (Time.time - startTime) * speed;
+		float progress = distCovered / totalDistance;
+		
+		if (isMoving){
+			transform.position = Vector3.Lerp(groundFloor.position, topFloor.position, progress);
+		}
+
+		if (Mathf.Abs(progress) > .999 && isMoving){
+			isMoving = false;
+		}
+	}
+}
